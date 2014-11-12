7fcb30624a1aad0c267a06a04d3a9f173162d1f9
Sam Clopton <samsinite@gmail.com>
2014-11-12 10:21:24 -0800

    Update template.rb

--- template.rb
+++ template.rb
@@ -66,9 +66,9 @@ ember_app = "#{@app_name}-ember"
 # create ember-cli app
 run "ember new #{ember_app}"
 
-create_file ".nvmrc" do
-  "0.10.32"
-end
+# create_file ".nvmrc" do
+#   "0.10.32"
+# end
 
 # Create the file that sets the default ember serve options (like the proxy)
 file "#{ember_app}/.ember-cli", <<-FILE
